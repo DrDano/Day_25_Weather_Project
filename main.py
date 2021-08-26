a1e90566b165d219b141591d861295bee15324d0
DrDano <danielharned@gmail.com>
2021-08-26 16:55:10 -0700

    Created temperatures list (Day 25 100DoC) 082621

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-data = []
+import csv
 
+data_list = []
+# with open("weather_data.csv", mode="r") as weather_file:
+#     data_0 = weather_file.readlines()
+#     for line in data_0:
+#         data.append(line[:-1])
+temperatures = []
 with open("weather_data.csv", mode="r") as weather_file:
-    data_0 = weather_file.readlines()
-    for line in data_0:
-        data.append(line[:-1])
-
-print(data)
+    data = csv.reader(weather_file)
+    for line in data:
+        data_list.append(line[1])
+        new_list = data_list[1:]
+    for index in new_list:
+        temperatures.append(int(index))
+print(temperatures)
