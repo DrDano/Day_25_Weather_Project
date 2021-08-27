dc4e81fffaba442b4aa3bd17666f0304bddbd464
DrDano <danielharned@gmail.com>
2021-08-27 12:00:56 -0700

    Imported Pandas and created temperature column (Day 25 100DoC) 082721

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ data_list = []
 #     data_0 = weather_file.readlines()
 #     for line in data_0:
 #         data.append(line[:-1])
-temperatures = []
-with open("weather_data.csv", mode="r") as weather_file:
-    data = csv.reader(weather_file)
-    for line in data:
-        data_list.append(line[1])
-        new_list = data_list[1:]
-    for index in new_list:
-        temperatures.append(int(index))
-print(temperatures)
+
+# temperatures = []
+# with open("weather_data.csv", mode="r") as weather_file:
+#     data = csv.reader(weather_file)
+#     for line in data:
+#         data_list.append(line[1])
+#         new_list = data_list[1:]
+#     for index in new_list:
+#         temperatures.append(int(index))
+# print(temperatures)
+
+import pandas
+data = pandas.read_csv(filepath_or_buffer="weather_data.csv")
+print(data["temp"])
